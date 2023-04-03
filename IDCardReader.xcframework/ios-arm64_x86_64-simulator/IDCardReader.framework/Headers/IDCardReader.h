2b484e173b6128679d6f2ba5ebb6e76db76d1ff9
EricNguyen <trungnguyen.1791@gmail.com>
2023-04-03 23:12:03 +0700

    no message

---
+++ IDCardReader.xcframework/ios-arm64_x86_64-simulator/IDCardReader.framework/Headers/IDCardReader.h
@@ -0,0 +1,18 @@
+//
+//  IDCardReader.h
+//  IDCardReader
+//
+//  Created by admin on 24/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDCardReader.
+FOUNDATION_EXPORT double IDCardReaderVersionNumber;
+
+//! Project version string for IDCardReader.
+FOUNDATION_EXPORT const unsigned char IDCardReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDCardReader/PublicHeader.h>
+
+
